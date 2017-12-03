004ecfb94a3426e7394e28a1c1f22b522a751643
seojiwoong <king0870@naver.com>
2017-12-03 11:41:50 +0900

    coin.js 들여쓰기 수정

--- scripts/coin.js
+++ scripts/coin.js
@@ -9,6 +9,7 @@ const getPrice = (coinType, cb)=>{
 	}
 
 	const url =`https://api.korbit.co.kr/v1/ticker/detailed?currency_pair=${coinNameToParam[coinType]}`
+
 	request.get({
 		url:url,
 		headers:{
@@ -18,15 +19,15 @@ const getPrice = (coinType, cb)=>{
 		if (err) {
 			return cb(err, null);
 		} else if (resp.statusCode !== 200){
-       	    return cb('시세를 못찾겠네여...', null);
-		}else {
-		    let result = JSON.parse(res);
+			return cb('시세를 못찾겠네여...', null);
+		} else {
+			let result = JSON.parse(res);
 	
-    	    if( !result || !result.bid || !result.ask){
-        	    return cb('시세를 못찾겠네여...', null);
-	        } else {
+			if ( !result || !result.bid || !result.ask) {
+        		return cb('시세를 못찾겠네여...', null);
+			} else {
 				let message = `현재 ${coinType}의 최종거래가격은 ${result.last}원 매수가격은 ${result.bid}원 매도가격은 ${result.ask}원 입니다 감사합니다`
-	    	    return cb(null, message)
+				return cb(null, message)
 			}
 		}
 	});    
